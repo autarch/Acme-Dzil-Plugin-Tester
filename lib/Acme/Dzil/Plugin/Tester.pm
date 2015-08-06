a3f2e8391a8891af37172e1a1bdfa865727553e2
Dave Rolsky <autarch@urth.org>
2015-08-06 14:10:10 -0400

    Initial commit

---
+++ lib/Acme/Dzil/Plugin/Tester.pm
@@ -0,0 +1,21 @@
+package Acme::Dzil::Plugin::Tester;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+# ABSTRACT: A distro that exists so I can test various dzil plugins
+
+__END__
+
+=pod
+
+=head1 DESCRIPTION
+
+It's really hard to test certain dzil plugins (especially release plugins)
+without actually doing a release. This distro lets me test them.
+
+=cut
